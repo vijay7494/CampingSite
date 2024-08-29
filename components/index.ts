993c3d0fac87618a86935a40556a164bc364b292
Vijay Chaure <vijaychaure7494@gmail.com>
2024-08-30 00:09:15 +0530

    Camping Site v1

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "./Button";
+import Hero from "./Hero";
+import Guide from "./Guide";
+import Features from "./Features";
+import GetApp from "./GetApp";
+import Camp from "./Camp";
+
+export {
+    Navbar,
+    Footer,
+    Button,
+    GetApp,
+    Features,
+    Guide,
+    Hero,
+    Camp
+};
---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export interface ButtonProps {
+    title?:string;
+    type:'button' | 'submit';
+    icon?: string;
+    variant: string;
+    full?:boolean;
+};
